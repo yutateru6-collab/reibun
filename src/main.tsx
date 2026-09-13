3d46ce5327072c10009c365d5654d8e815e9e538
yutateru6-collab <yutateru6@gmail.com>
2026-09-13 23:53:40 +0900

    Mount Japanese tense knowledge practice without changing existing study modes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import ExamRangeGuide from './ExamRangeGuide.tsx';
 import Workbook from './workbook/Workbook';
+import KnowledgeQuiz from './knowledge/KnowledgeQuiz';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')!).render(
     <App />
     <ExamRangeGuide />
     <Workbook />
+    <KnowledgeQuiz />
   </StrictMode>,
 );
